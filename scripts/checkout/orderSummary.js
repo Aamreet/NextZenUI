2d100eba4660ccac4a129f76699e98d6da4258ea
Aamreet <amreetsingh508@gmail.com>
2023-12-15 11:19:58 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,7 +3,7 @@ import {products, getProduct} from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
+import { rernderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
   let cartSummaryHtml = " ";
@@ -113,8 +113,10 @@ export function renderOrderSummary(){
         const productId = link.dataset.productId;
         removeFromCart(productId);
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
-        container.remove();
-
+        container.remove();// this is dom manupulatin it will update the page but we need to reload it 
+                           // to regernate html
+  
+         rernderPaymentSummary();// we are doing what reload will do so that we could see the result on the page without reloading
         });
   });
 
@@ -124,6 +126,8 @@ export function renderOrderSummary(){
         const { productId, deliveryOptionId}= element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+
+        rernderPaymentSummary();
     });
   });
 
